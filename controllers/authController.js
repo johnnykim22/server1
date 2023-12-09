1e0988df71ce1f108cefc874f3dcbed4b3d692fa
johnnykim22 <johnnykim22@gmail.com>
2023-12-08 17:14:56 -0800

    add build script

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
