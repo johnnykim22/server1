31a8eb64b5ba39c3ab20c7e73e87e9b1fc15e23a
johnnykim22 <johnnykim22@gmail.com>
2023-12-12 19:03:46 -0800

    new final

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://authform-cdgoxf2wy-john-kims-projects.vercel.app",
+  "https://authform-gamma.vercel.app/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
